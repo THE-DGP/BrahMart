a524e0cbdc39c23f4368341043d5eeb93cc5cdbe
Dev Patel <88072755+THE-DGP@users.noreply.github.com>
2024-04-22 10:31:48 +0530

    Update App.py

--- App.py
+++ App.py
@@ -36,11 +36,11 @@ import getpass
 # os.environ['GOOGLE_API_KEY'] = getpass.getpass('Gemini API Key:')
 
 # engine=pyttsx3.init()
-embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key='AIzaSyAewIz-dDslyp-XE28hvojOJny6MTBd5rE')
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=GOOGLE_API_KEY)
 x = FAISS.load_local("final_faiss_index", embeddings, allow_dangerous_deserialization=True)
 
 def get_conversation_chain(x):
-    llm = ChatOpenAI(model="gpt-3.5-turbo",openai_api_key='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')
+    llm = ChatOpenAI(model="gpt-3.5-turbo",openai_api_key=OPENAI_API_KEY)
     memory = ConversationBufferMemory(memory_key="chat_history", input_key="question", output_key="answer",return_messages=True)
     system_template = r"""
     You're a helpful e-commerce AI assistant. Given a user description about a product or pricing or color and other attributes. Answer the questions correctly. If the query is not related to the product just say that you didn't find any relevant product.
@@ -96,8 +96,7 @@ def submit():
         st.conversation = get_conversation_chain(x)
         handle_user_input(st.text_received)
 def submit1():
-        # st.text_received=st.session_state.widget
-        # st.session_state.widget=""
+      
         st.conversation = get_conversation_chain(x)
         handle_user_input(st.text_received)
         
@@ -123,11 +122,7 @@ def main():
         inp1=st.text_area(label="Your Query",value=text,key="widget", disabled=True)
         st.button(on_click=submit1, label="Submit")
         
-        # if text:
-        #     st.write("User has sent a prompt", inp)
-        #     engine.say(inp)
-        # for text in state.text_received:
-        #     st.text(text)
+        
             
     elif st.option=='Text':
          inp=st.text_input(label="Your Query",key='widget',on_change=submit, value='')
@@ -149,32 +144,7 @@ def display_images_in_sidebar(image_data):
 # Button to show popular products
 if st.sidebar.button('Show Popular Products'):
     display_images_in_sidebar(image_data)
-# image_data = [
-#     {"title": "Fuson Back Cover for Samsung Galaxy J7", "image_url": "http://img6a.flixcart.com/image/cases-covers/back-cover/f/d/f/fuson-3d-vk-ipadmini2-d9516-1100x1100-imaedm45vhjfhhys.jpeg"},
-#     {"title": "Candy House Solid Men's Polo Neck T-Shirt", "image_url": "http://img6a.flixcart.com/image/t-shirt/v/e/h/polo3-red-ylo-gry-christy-world-m-1000x1000-imaegj44wr7cg5fx.jpeg"},
-#     {"title": "Mynte Solid Women's Cycling Shorts, Gym Shorts, Swim Shorts", "image_url": "http://img6a.flixcart.com/image/watch/t/j/a/77036sm02j-sonata-original-imaecqsfpxqeyhed.jpeg"},
-#     {"title": "Kiara Jewellery Sterling Silver Cubic Zirconia Rhodium Ring", "image_url": "http://img5a.flixcart.com/image/ring/h/j/8/kir0195-8-kiara-jewellery-ring-original-imadtdy7yhyfgvxw.jpeg"},
-#     {"title": "kasemantra Back Cover for Apple iPad Mini", "image_url": "http://img6a.flixcart.com/image/cases-covers/back-cover/d/h/z/kasemantra-kasemantra-confused-face-case-for-apple-ipad-mini-1100x1100-imae6pu2z6nheqzt.jpeg"},
-#     {"title": "Rr Rainbow Grand Dlx 30 L Backpack", "image_url": "http://img5a.flixcart.com/image/backpack/y/r/n/rbbp00104-mb-rr-rainbow-backpack-grand-dlx-original-imaecsv6hyzh7yqz.jpeg"}    
-# ]
 
-# # Function to display images in a grid
-# def display_images_in_grid(image_data):
-#     # Create 4 columns
-#     cols = st.columns(3)
-#     # Display images in a 4x2 grid
-#     for index, image_info in enumerate(image_data):
-#         with cols[index % 3]:
-#             st.image(image_info["image_url"], caption=image_info["title"], width=150)
-
-# # Button to show popular products
-# if st.button('Show Popular Products'):
-#     display_images_in_grid(image_data)
-# # st.write("Record your voice, and play the recorded audio:")
-# audio = mic_recorder(start_prompt="", stop_prompt="", key='recorder')
-
-# if audio:
-#     st.audio(audio['bytes'])
 
 if __name__ == "__main__":
     main()
